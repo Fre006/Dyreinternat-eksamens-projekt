f2c48407239511feac813f6541c4633c61f8f0b5
Bjarke Erik Hjalmar Kusk Arvedlund <bja001@edu.zealand.dk>
2025-05-15 13:00:48 +0200

    ICatRepo And CatJsonRepo

--- I_Lib_For_The_Dyreinternat/Repo/CatJsonRepo.cs
+++ I_Lib_For_The_Dyreinternat/Repo/CatJsonRepo.cs
@@ -1,12 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using Lib.Model;
 
 namespace Lib.Repo
 {
     internal class CatJsonRepo
     {
+        public string Path="Cat.json";
+        protected List<Cat> _cats = new List<Cat>();
+
+        public void CatJSONRepository()
+        {
+            LoadFile(Path);
+        }
+
+        public List<Cat> GetAll()
+        {
+            return _cats;
+        }
+
+        public void Add(Cat cat)
+        {
+            _cats.Add(cat);
+            SaveFile(Path);
+        }
+
+
+        //denne metode skal kaldes hver gang vi gerne vil trække data fra vores JSON
+        private void LoadFile()
+        {
+            string json = File.ReadAllText(Path);
+
+            _cats = JsonSerializer.Deserialize<List<Cat>>(json);
+        }
+
+        //denne metode skal kaldes når vi vil putte data i vores JSON
+        private void SaveFile()
+        {
+
+            File.WriteAllText(Path, JsonSerializer.Serialize(_cats));
+        }
     }
 }
---
+++ I_Lib_For_The_Dyreinternat/Repo/ICatRepo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using Lib.Model;
+
+namespace Lib.Repo
+{
+    internal interface ICatRepo
+    {
+        public List<Cat> GetAll();
+        public void Add(Cat cat, string path);
+    }
+}
