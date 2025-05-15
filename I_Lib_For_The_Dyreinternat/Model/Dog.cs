bcc58a3ed5c361da12d893337812062ca4fe7828
Bjarke Erik Hjalmar Kusk Arvedlund <bja001@edu.zealand.dk>
2025-05-15 12:38:27 +0200

    removed default constructor

--- I_Lib_For_The_Dyreinternat/Model/Dog.cs
+++ I_Lib_For_The_Dyreinternat/Model/Dog.cs
@@ -11,8 +11,21 @@ namespace Lib.Model
     internal class Dog:Animal
     {
         public DogBreeds Breed; 
-        public Dog(DogBreeds breed) { 
-        Breed= breed;
+        public Dog(DogBreeds breed, string name, string characteristics, string status, bool male, bool fertile, Sizes size, List<string> logs, string chipID, string description, DateTime birthdate) { 
+            Breed= breed;
+            Name = name;
+            Characteristics = characteristics;
+            Status = status;
+            Male = male;
+            Fertile = fertile;
+            Size = size;
+            ChipID = chipID;
+            Description = description;
+            Logs = logs;
+            ChipID = chipID;
+            Birthdate = birthdate;
+
+
         }
 
 
