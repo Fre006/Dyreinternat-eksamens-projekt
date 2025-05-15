5f16227ad502ce30dc9c7ce692801a8ffb2ef933
Frederick Grønbech Kure Ertel <fre006@edu.zealand.dk>
2025-05-15 14:28:22 +0200

    Vet. Inteface og repo

---
+++ I_Lib_For_The_Dyreinternat/Repo/VeterinarianJSONRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Lib.Model;
+
+namespace Lib.Repo
+{
+    internal class VeterinarianJSONRepository : IVeterinarianJSONRepository
+    {
+        public VeterinarianJSONRepository()
+        {
+            LoadFile();
+        }
+
+        //denne metode skal kaldes hver gang vi gerne vil trække data fra vores JSON
+        private void LoadFile()
+        {
+            string path = "Veterinarian.json";
+            string json = File.ReadAllText(path);
+
+            _veterinarian = JsonSerializer.Deserialize<List<VeterinarianVisit>>(json);
+        }
+
+        public virtual void Add(VeterinarianVisit veterinarian)
+        {
+            _veterinarian.Add(veterinarian);
+            SaveFile();
+        }
+
+        //denne metode skal kaldes når vi vil putte data i vores JSON
+        private void SaveFile()
+        {
+            string path = "Veterinarian.json";
+            File.WriteAllText(path, JsonSerializer.Serialize(_veterinarian));
+        }
+
+        public List<VeterinarianVisit> _veterinarian = new List<VeterinarianVisit>();
+
+        public List<VeterinarianVisit> GetAll()
+        {
+            return _veterinarian;
+        }
+    }
+}
