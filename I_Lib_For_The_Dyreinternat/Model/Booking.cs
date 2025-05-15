b94d7b0661621dcf0d0cc0dbf653e9a56c533796
Frederick Grønbech Kure Ertel <fre006@edu.zealand.dk>
2025-05-15 15:05:38 +0200

    Booking Models, repo og interface

---
+++ I_Lib_For_The_Dyreinternat/Model/Booking.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Lib.Model
+{
+    internal class Booking : Event
+    {
+        public Booking(string name, string description, int costumerCap, int animalCap, List<Costumer> costumers, List<Animal> animals, List<Worker> workers, string location, DateTime start, DateTime stop)
+        {
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name}  Description: {Description}  CostumerCap: {CostumerCap}  AnimalCap: {AnimalCap}  Costumers: {Costumers} Animals: {Animals} Workers: {Workers} Location: {Location} Time start: {Start} time stop: {Stop}";
+        }
+    }
+}
---
+++ I_Lib_For_The_Dyreinternat/Repo/BookingJSONRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Lib.Model;
+
+namespace Lib.Repo
+{
+    internal class BookingJSONRepository : IBookingJSONRepository
+    {
+        public BookingJSONRepository()
+        {
+            LoadFile();
+        }
+
+        //denne metode skal kaldes hver gang vi gerne vil trække data fra vores JSON
+        private void LoadFile()
+        {
+            string path = "Booking.json";
+            string json = File.ReadAllText(path);
+
+            _booking = JsonSerializer.Deserialize<List<Booking>>(json);
+        }
+
+        public virtual void Add(Booking booking)
+        {
+            _booking.Add(booking);
+            SaveFile();
+        }
+
+        //denne metode skal kaldes når vi vil putte data i vores JSON
+        private void SaveFile()
+        {
+            string path = "Booking.json";
+            File.WriteAllText(path, JsonSerializer.Serialize(_booking));
+        }
+
+        public List<Booking> _booking = new List<Booking>();
+
+        public List<Booking> GetAll()
+        {
+            return _booking;
+        }
+    }
+}
