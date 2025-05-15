ec763ed67aff61776c11379385bd703c3491046a
Bjarke Erik Hjalmar Kusk Arvedlund <bja001@edu.zealand.dk>
2025-05-15 12:43:37 +0200

    Fixed cat and dog

--- I_Lib_For_The_Dyreinternat/Model/Cat.cs
+++ I_Lib_For_The_Dyreinternat/Model/Cat.cs
@@ -11,20 +11,9 @@ namespace Lib.Model
     {
         public CatBreeds Breed;
 
-        public Cat(CatBreeds breed, string name, string characteristics, string status, bool male, bool fertile, Sizes size, List<string> logs, string chipID, string description, DateTime birthdate)
+        public Cat(CatBreeds breed)
         {
             Breed = breed;
-            Name = name;
-            Characteristics = characteristics;
-            Status = status;
-            Male = male;
-            Fertile = fertile;
-            Size = size;
-            ChipID = chipID;
-            Description = description;
-            Logs = logs;
-            ChipID = chipID;
-            Birthdate = birthdate;
         }
 
     }
--- I_Lib_For_The_Dyreinternat/Model/Dog.cs
+++ I_Lib_For_The_Dyreinternat/Model/Dog.cs
@@ -11,19 +11,9 @@ namespace Lib.Model
     internal class Dog:Animal
     {
         public DogBreeds Breed; 
-        public Dog(DogBreeds breed, string name, string characteristics, string status, bool male, bool fertile, Sizes size, List<string> logs, string chipID, string description, DateTime birthdate) { 
+        public Dog(DogBreeds breed) { 
             Breed= breed;
-            Name = name;
-            Characteristics = characteristics;
-            Status = status;
-            Male = male;
-            Fertile = fertile;
-            Size = size;
-            ChipID = chipID;
-            Description = description;
-            Logs = logs;
-            ChipID = chipID;
-            Birthdate = birthdate;
+
 
 
         }
