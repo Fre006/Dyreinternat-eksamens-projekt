cca88174f6f5c18fdbfeb762fa515b56b36fb6c4
Bjarke Erik Hjalmar Kusk Arvedlund <bja001@edu.zealand.dk>
2025-05-15 11:13:03 +0200

    dog properties

--- I_Lib_For_The_Dyreinternat/Model/Dog.cs
+++ I_Lib_For_The_Dyreinternat/Model/Dog.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace I_Lib_For_The_Dyreinternat.Model
 {
     internal class Dog
     {
+        public enum Race;
+
+
     }
 }
