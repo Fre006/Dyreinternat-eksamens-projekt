35055ff0838e62dd74e0377eef276a33501f4d26
Frederick Grønbech Kure Ertel <fre006@edu.zealand.dk>
2025-05-18 19:11:25 +0200

    A bit of UI but i ran into a problem of knowledge using external classes

---
+++ Dyreinternat eksamens projekt/Pages/Create Event.cshtml.cs
@@ -0,0 +1,34 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dyreinternat_eksamens_projekt.Pages
+{
+    public class Create_EventModel : PageModel
+    {
+        //private PetService _petService;
+
+        //[BindProperty]
+        //public Pet Pet { get; set; }
+
+        //public CreateModel(PetService ps)
+        //{
+        //    Pet = new Pet();
+
+        //    _petService = ps;
+        //}
+
+        public void OnGet()
+        {
+
+        }
+
+        //public IActionResult OnPost()
+        //{
+        //    Debug.WriteLine("test" + Pet.Age);
+        //    _petService.Add(Pet);
+        //    return RedirectToPage("/Index");
+        //}
+
+    }
+}
