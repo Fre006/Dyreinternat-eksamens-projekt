d080fa5315dee5ee53083b3aa57dcd6b37921e99
Frederick Grønbech Kure Ertel <fre006@edu.zealand.dk>
2025-05-20 16:04:04 +0200

    Start på Vault klasserne

---
+++ I_Lib_For_The_Dyreinternat/Repo/VaultAnimalJSONRepo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Lib.Model;
+
+namespace Lib.Repo
+{
+    internal class VaultAnimalJSONRepo
+    {
+        public Dictionary<int, Animal> _oldAnimal = new Dictionary<int, Animal>();
+        private void LoadFile()
+        {
+            string path = "oldAnimal.json";
+            string json = File.ReadAllText(path);
+
+            _oldAnimal = JsonSerializer.Deserialize<Dictionary<int, Animal>>(json);
+        }
+        private void SaveFile()
+        {
+            string path = "oldEvents.json";
+            File.WriteAllText(path, JsonSerializer.Serialize(_oldAnimal));
+        }
+
+    }
+}
