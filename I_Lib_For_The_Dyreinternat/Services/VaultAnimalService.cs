7f7bf3fb0b33486e04c90c00bb5b6b2e18758de1
Frederick Grønbech Kure Ertel <fre006@edu.zealand.dk>
2025-05-21 15:48:21 +0200

    Vault Services

---
+++ I_Lib_For_The_Dyreinternat/Services/VaultAnimalService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lib.Repo;
+using Lib.Model;
+
+namespace Lib.Services
+{
+    internal class VaultAnimalService
+    {
+        private IVaultAnimalJSONRepo _VaultAnimalRepo;
+        public VaultAnimalService(IVaultAnimalJSONRepo VaultAnimalRepo)
+        {
+            _VaultAnimalRepo = VaultAnimalRepo;
+        }
+        public void VaultAnimal(Animal oldAnimal)
+        {
+            _VaultAnimalRepo.VaultAnimal(oldAnimal);
+        }
+        public Animal VaultGetAnimal(string Key)
+        {
+            return _VaultAnimalRepo.VaultGetAnimal(Key);
+        }
+    }
+}
