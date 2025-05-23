03269f7b6f477062f09b7b8a1740571521ff2ea9
Patrick Bachmann Vanting Jarshøj <paj003@edu.zealand.dk>
2025-05-23 10:41:29 +0200

    Made blog site work with deletion button

--- Dyreinternat eksamens projekt/Pages/Blog.cshtml.cs
+++ Dyreinternat eksamens projekt/Pages/Blog.cshtml.cs
@@ -4,17 +4,51 @@ using Lib.Model;
 using Lib.Repo;
 using Lib.Services;
 using Dyreinternat_eksamens_projekt.Pages;
+using System.Diagnostics;
+using System.Net.Security;
 
 namespace Dyreinternat_eksamens_projekt.Pages
 {
     public class BlogModel : PageModel
     {
-        //public Blog SpecificBlog;
+        [BindProperty]
+        public Blog SpecificBlog { get; set; }
+        [BindProperty]
+        public string TempTitle { get; set; }
+        [BindProperty]
+        public bool Edit { get; set; } = false;
+        [BindProperty]
+        public string Title { get; set; }
+        [BindProperty]
+        public string Text { get; set; }
+        [BindProperty]
+        public Worker Author { get; set; }
+        [BindProperty]
+        public string AuthorName { get; set; }
 
         BlogService _blogService;
-        public void OnGet(BlogService bs)
+        public BlogModel(BlogService bs)
         {
             _blogService = bs;
+        }
+
+        public IActionResult OnPostDelete()
+        {
+            _blogService.Delete(TempTitle);
+            return RedirectToPage("/CreateBlog");
+        }
+
+        public void OnPostEdit()
+        {
+            Edit = true;
+        }
+
+
+        public void OnGet(string title)
+        {
+            //Debug.WriteLine("Hello: " + title);
+
+            SpecificBlog = _blogService.GetByTitle(title);
         }
     }
 }
